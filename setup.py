b3da2c08f5984a037bec23bc04b3993fa53c5509
翻滚的葫芦球 <alezaizai@163.com>
2017-01-18 20:07:44 +0800

    feat(pypi): add setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='gotyou',
+    version='0.1',
+    description='a simple crawler now',
+    author='alezai',
+    author_email='alezaizai@163.com',
+    url='https://github.com/huluqiu/gotyou',
+    packages=['gotyou'],
+
+    isntall_requires=['requests', 'lxml'],
+)
