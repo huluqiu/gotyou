63a2db4144341fc91a93d6dabc319cb3641b4058
翻滚的葫芦球 <yongle.lin@outlook.com>
2017-03-02 20:24:47 +0800

    fix(install_requires): error spell

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='gotyou',
-    version='0.1.2',
+    version='0.1.3',
     description='a simple crawler now',
     author='alezai',
     author_email='alezaizai@163.com',
     url='https://github.com/huluqiu/gotyou',
     packages=['gotyou'],
 
-    isntall_requires=['requests', 'lxml'],
+    install_requires=['requests', 'lxml'],
 )
